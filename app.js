85679dd8e5e376d1a2e524e80a35ddb14062d1c9
Molly Montgomery <Molly@mollys-air-2.lan>
2021-12-02 16:04:00 -0600

    added inquirer

---
+++ app.js
@@ -0,0 +1,20 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+// const pageHTML = generatePage(name, github);
+
+//fs.writeFile('./index.html', pageHTML, err => {
+//  if (err) throw err;
+
+ // console.log('Portfolio complete! Check out index.html to see the output!');
+// });
